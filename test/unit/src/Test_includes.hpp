23474099d84ef6083d5eb899f2a099929f2e11ea
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2024-10-25 20:45:37 +0530

    test/unit: Add first unit test as template.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

---
+++ test/unit/src/Test_includes.hpp
@@ -0,0 +1,15 @@
+#ifndef TEST_INCLUDES_HPP
+#define TEST_INCLUDES_HPP
+
+
+// Arduino includes
+#include <Arduino.h>
+
+// project cpp includes
+#include "Utilities.hpp"
+
+// test includes
+#include "Test_common_includes.h"
+
+
+#endif // TEST_INCLUDES_HPP
