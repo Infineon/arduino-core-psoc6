9be8c37f89db95103a3916a4d93fcf84b3a255bf
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2024-10-30 18:04:35 +0530

    test/unit/src: Add Math unit test.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

--- test/unit/src/utils/unity_ifx.cpp
+++ test/unit/src/utils/unity_ifx.cpp
@@ -8,15 +8,27 @@
 #include "unity_ifx.h"
 
 
+// extern "C" {  
+//     // Method used by Unity to output a single character 
+//     void putCharacter(char c) {
+//         Serial.print(c);
+//         Serial.flush();
+//     }
+
+//     // Method used by Unity to flush the output
+//     void flushCharacter() {
+//         Serial.flush();
+//     }
+// }
+
 extern "C" {  
     // Method used by Unity to output a single character 
     void putCharacter(char c) {
-        Serial.print(c);
-        Serial.flush();
+        printf(c);
     }
 
     // Method used by Unity to flush the output
     void flushCharacter() {
-        Serial.flush();
+        
     }
 }
