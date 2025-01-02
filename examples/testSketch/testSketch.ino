1cd9c36dbe170cb5114213520f1731adb09ec789
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2025-01-02 08:25:58 +0530

    examples/testSketch: Add example to toggle LED with delay functions.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

--- examples/testSketch/testSketch.ino
+++ examples/testSketch/testSketch.ino
@@ -1,18 +1,26 @@
+#include "cy_gpio.h" 
 
-double number;
-double result;
+// Pin definition for the LED
+#define LED_PORT GPIO_PRT5
+#define LED_PIN 3
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 
-  // Define a number to calculate the square root of
-  number = 25.0;
+  Cy_GPIO_Pin_FastInit(LED_PORT, LED_PIN, CY_GPIO_DM_STRONG_IN_OFF, 0, HSIOM_SEL_GPIO);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  // Loop to count from 0 to 100
-  for (int i = 0; i <= 100; i++) {
-    result = sqrt(number);
-  }
+  // Toggle the LED using delay
+  Cy_GPIO_Write(LED_PORT, LED_PIN, 1);
+  delay(1000); // Delay for 1000 milliseconds (1 second)
+  Cy_GPIO_Write(LED_PORT, LED_PIN, 0);
+  delay(1000); // Delay for 1000 milliseconds (1 second)
+
+  // Toggle the LED using delayMicroseconds
+  Cy_GPIO_Write(LED_PORT, LED_PIN, 1);
+  delayMicroseconds(500000); // Delay for 500000 microseconds (0.5 seconds)
+  Cy_GPIO_Write(LED_PORT, LED_PIN, 0);
+  delayMicroseconds(500000); // Delay for 500000 microseconds (0.5 seconds)
 }
