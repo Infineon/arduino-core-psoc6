2a64a5ce65fd15155c9c81970da8fe62f30c253e
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2025-01-10 05:54:33 +0530

    examples/testSketch: Update example to toggle LED with digital IO APIs.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

--- examples/testSketch/testSketch.ino
+++ examples/testSketch/testSketch.ino
@@ -1,26 +1,21 @@
-#include "cy_gpio.h" 
 
-// Pin definition for the LED
-#define LED_PORT GPIO_PRT5
-#define LED_PIN 3
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 
-  Cy_GPIO_Pin_FastInit(LED_PORT, LED_PIN, CY_GPIO_DM_STRONG_IN_OFF, 0, HSIOM_SEL_GPIO);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  // Toggle the LED using delay
-  Cy_GPIO_Write(LED_PORT, LED_PIN, 1);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000); // Delay for 1000 milliseconds (1 second)
-  Cy_GPIO_Write(LED_PORT, LED_PIN, 0);
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000); // Delay for 1000 milliseconds (1 second)
 
   // Toggle the LED using delayMicroseconds
-  Cy_GPIO_Write(LED_PORT, LED_PIN, 1);
+  digitalWrite(LED_BUILTIN, HIGH);
   delayMicroseconds(500000); // Delay for 500000 microseconds (0.5 seconds)
-  Cy_GPIO_Write(LED_PORT, LED_PIN, 0);
+  digitalWrite(LED_BUILTIN, LOW);
   delayMicroseconds(500000); // Delay for 500000 microseconds (0.5 seconds)
 }
