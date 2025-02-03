813ae0a9c94c15862b2a856d0162341f8a167222
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-02-03 17:43:58 +0100

    libraries/WiFi/src/WiFiServer: Refactored based on Socket class.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- libraries/WiFi/src/WiFiServer.h
+++ libraries/WiFi/src/WiFiServer.h
@@ -1,8 +1,11 @@
+#ifndef WIFI_SERVER_H
+#define WIFI_SERVER_H
+
 #include <stdint.h>
 #include <vector>
 #include <WiFiClient.h>
+#include <Socket.h>
 #include "Server.h"
-#include "cy_secure_sockets.h"
 
 class WiFiServer : public arduino::Server {
 
@@ -17,21 +20,20 @@ class WiFiServer : public arduino::Server {
 
         void end();
 
+        static uint32_t connect_callback_count;
+
     private:
 
-        #define TCP_SERVER_RECV_TIMEOUT_MS  (500u)
-        
-        cy_socket_sockaddr_t address;
-        cy_socket_t socket;
+        static const uint16_t SERVER_RECV_TIMEOUT_MS = 500;
+        static const uint32_t SERVER_MAX_CLIENTS = 255;
+
+        Socket socket;
 
         std::vector <WiFiClient> connected_clients;
-
-        void set_socket_timeout(uint32_t timeout);
-        void set_socket_connect_opts();
-        void set_socket_receive_opts();
-        void set_socket_disconnect_opts();
 
         static cy_rslt_t tcp_connection_handler(cy_socket_t socket_handle, void *arg);
         static cy_rslt_t tcp_receive_msg_handler(cy_socket_t socket_handle, void *arg);
         static cy_rslt_t tcp_disconnection_handler(cy_socket_t socket_handle, void *arg);
 };
+
+#endif /* WIFI_SERVER_H */
