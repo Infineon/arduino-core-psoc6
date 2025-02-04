e3be636d8650c3a622863e907625339880a0fa81
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-02-04 18:05:05 +0100

    library/WiFi/src/Socket: Added status and error tracking.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- libraries/WiFi/src/Socket.cpp
+++ libraries/WiFi/src/Socket.cpp
@@ -1,6 +1,7 @@
 #include <Socket.h>
 
 #define socket_assert(cy_ret)   if (cy_ret != CY_RSLT_SUCCESS) { \
+        s_status = SOCKET_STATUS_ERROR; \
         return; \
 }
 
@@ -8,45 +9,48 @@
         return cy_ret; \
 }
 
-Socket::Socket(): socket(nullptr), status(CY_RSLT_SUCCESS) {
+Socket::Socket(): 
+socket(nullptr), 
+s_status(SOCKET_STATUS_UNINITED),
+s_last_error(CY_RSLT_SUCCESS) {
 
 }
 
 void Socket::begin() {
-    cy_rslt_t ret;
-    ret = Socket::global_sockets_begin();
-    set_status(ret);
-    socket_assert(ret);
 
-    ret = cy_socket_create(CY_SOCKET_DOMAIN_AF_INET, CY_SOCKET_TYPE_STREAM,
+    s_last_error = Socket::global_sockets_begin();
+    socket_assert(s_last_error);
+
+    s_last_error = cy_socket_create(CY_SOCKET_DOMAIN_AF_INET, CY_SOCKET_TYPE_STREAM,
                               CY_SOCKET_IPPROTO_TCP, &socket);
-    set_status(ret);
+    socket_assert(s_last_error);
+
+    s_status = SOCKET_STATUS_CREATED;
 }
 
 void Socket::end() {
-    cy_rslt_t ret;
     /** 
      * TODO: This sequence of disconnect delete requires review
      * { 
      */
-    ret = cy_socket_disconnect(socket, 0);
-    set_status(ret);
-    socket_assert(ret);
+    s_last_error = cy_socket_disconnect(socket, 0);
+    socket_assert(s_last_error);
     /* } */
+    s_status = SOCKET_STATUS_DISCONNECTED;
 
-    ret = cy_socket_delete(socket);
-    set_status(ret);
-    socket_assert(ret);
+    s_last_error = cy_socket_delete(socket);
+    socket_assert(s_last_error);
+    s_status = SOCKET_STATUS_CLOSED;
 
-    ret = Socket::global_sockets_end();
+    s_last_error = Socket::global_sockets_end();
 
 }
 
 void Socket::set_timeout(uint32_t timeout) {
-    cy_rslt_t ret = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
+    s_last_error = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
                                     CY_SOCKET_SO_RCVTIMEO, &timeout,
-                                    sizeof(timeout));
-    set_status(ret);              
+                                    sizeof(timeout)); 
+    socket_assert(s_last_error);
 }
 
 void Socket::set_connect_opts(cy_socket_callback_t cback, void *arg) {   
@@ -55,10 +59,10 @@ void Socket::set_connect_opts(cy_socket_callback_t cback, void *arg) {
     tcp_connection_option.callback = cback;
     tcp_connection_option.arg = arg;
 
-    cy_rslt_t ret = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
+    s_last_error = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
                                     CY_SOCKET_SO_CONNECT_REQUEST_CALLBACK,
                                     &tcp_connection_option, sizeof(cy_socket_opt_callback_t));
-    set_status(ret); 
+    socket_assert(s_last_error);
 }
 
 void Socket::set_receive_opts(cy_socket_callback_t cback, void *arg) {
@@ -67,10 +71,10 @@ void Socket::set_receive_opts(cy_socket_callback_t cback, void *arg) {
     tcp_receive_option.callback = cback;
     tcp_receive_option.arg = arg;
 
-    cy_rslt_t ret = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
+    s_last_error = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
                                     CY_SOCKET_SO_RECEIVE_CALLBACK,
                                     &tcp_receive_option, sizeof(cy_socket_opt_callback_t));
-    set_status(ret); 
+    socket_assert(s_last_error);
 }
 
 void Socket::set_disconnect_opts(cy_socket_callback_t cback, void *arg) {
@@ -79,10 +83,10 @@ void Socket::set_disconnect_opts(cy_socket_callback_t cback, void *arg) {
     tcp_disconnection_option.callback = cback;
     tcp_disconnection_option.arg = arg;
 
-    cy_rslt_t ret = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
+    s_last_error = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
                                     CY_SOCKET_SO_DISCONNECT_CALLBACK,
                                     &tcp_disconnection_option, sizeof(cy_socket_opt_callback_t));
-    set_status(ret); 
+    socket_assert(s_last_error); 
 }
 
 void Socket::bind(uint16_t port) {
@@ -94,8 +98,10 @@ void Socket::bind(uint16_t port) {
         }
     };
 
-    cy_rslt_t ret = cy_socket_bind(socket, &address, sizeof(cy_socket_sockaddr_t));
-    set_status(ret);
+    s_last_error = cy_socket_bind(socket, &address, sizeof(cy_socket_sockaddr_t));
+    socket_assert(s_last_error);
+
+    s_status = SOCKET_STATUS_BOUND;
 }
 
 bool Socket::connect(IPAddress ip, uint16_t port) {
@@ -108,18 +114,20 @@ bool Socket::connect(IPAddress ip, uint16_t port) {
         }
     };
 
-    cy_rslt_t ret = cy_socket_connect(socket, &address, sizeof(cy_socket_sockaddr_t));
-    set_status(ret);
-    if(ret != CY_RSLT_SUCCESS) {
+    s_last_error = cy_socket_connect(socket, &address, sizeof(cy_socket_sockaddr_t));
+    if(s_last_error != CY_RSLT_SUCCESS) {
         return false;
     }
 
+    s_status = SOCKET_STATUS_CONNECTED;
     return true;
 }
 
 void Socket::listen(int max_connections) {
-    cy_rslt_t ret = cy_socket_listen(socket, max_connections);
-    set_status(ret);
+    s_last_error = cy_socket_listen(socket, max_connections);
+    socket_assert(s_last_error);
+
+    s_status = SOCKET_STATUS_LISTENING;
 }
 
 bool Socket::accept(Socket & client_socket) {
@@ -129,21 +137,22 @@ bool Socket::accept(Socket & client_socket) {
     cy_socket_sockaddr_t peer_addr;
     uint32_t peer_addr_len;
 
-    cy_rslt_t ret = cy_socket_accept(socket, &peer_addr, &peer_addr_len, &(client_socket.socket));
-    set_status(ret);
-    if(ret != CY_RSLT_SUCCESS) {
+    s_last_error = cy_socket_accept(socket, &peer_addr, &peer_addr_len, &(client_socket.socket));
+    if(s_last_error != CY_RSLT_SUCCESS) {
+        s_status = SOCKET_STATUS_ERROR;
         return false;
     }
-
+    
+    client_socket.s_status = SOCKET_STATUS_CONNECTED;
     return true;
 }
 
-int Socket::get_status() {
-    return status;
+uint8_t Socket::status() {
+    return s_status;
 }   
 
-void Socket::set_status(cy_rslt_t ret) {
-    status = ret;
+cy_rslt_t Socket::get_last_error() {
+    return s_last_error;
 }   
 
 bool Socket::global_socket_begun = false;
