e1b58e0e6c52e377d7c2decc64f31c23da8c3335
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-02-21 22:28:50 +0100

    libraries/WiFi: Completed wifi server and client without secure.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- libraries/WiFi/src/Socket.cpp
+++ libraries/WiFi/src/Socket.cpp
@@ -1,12 +1,8 @@
 #include <Socket.h>
 
 #define socket_assert(cy_ret)   if (cy_ret != CY_RSLT_SUCCESS) { \
-        s_status = SOCKET_STATUS_ERROR; \
+        _status = SOCKET_STATUS_ERROR; \
         return; \
-}
-
-#define socket_status_update(cy_ret)   if (cy_ret != CY_RSLT_SUCCESS) { \
-        s_status = SOCKET_STATUS_ERROR; \
 }
 
 #define socket_assert_raise(cy_ret)   if (cy_ret != CY_RSLT_SUCCESS) { \
@@ -15,44 +11,41 @@
 
 Socket::Socket(): 
 socket(nullptr), 
-s_status(SOCKET_STATUS_UNINITED),
-s_last_error(CY_RSLT_SUCCESS) {
+_status(SOCKET_STATUS_UNINITED),
+_last_error(CY_RSLT_SUCCESS),
+remote_ip(0, 0, 0, 0),
+_port(0) {
 
 }
 
 void Socket::begin() {
-    s_last_error = Socket::global_sockets_init();
-    socket_assert(s_last_error);
+    _last_error = Socket::global_sockets_init();
+    socket_assert(_last_error);
 
-    s_last_error = cy_socket_create(CY_SOCKET_DOMAIN_AF_INET, CY_SOCKET_TYPE_STREAM,
+    _last_error = cy_socket_create(CY_SOCKET_DOMAIN_AF_INET, CY_SOCKET_TYPE_STREAM,
                               CY_SOCKET_IPPROTO_TCP, &socket);
-    socket_assert(s_last_error);
+    socket_assert(_last_error);
 
-    s_status = SOCKET_STATUS_CREATED;
+    _status = SOCKET_STATUS_CREATED;
 }
 
 void Socket::end() {
-    /** 
-     * TODO: This sequence of disconnect delete requires review
-     * { 
-     */
-    s_last_error = cy_socket_disconnect(socket, 0);
-    socket_assert(s_last_error);
-    /* } */
-    s_status = SOCKET_STATUS_DISCONNECTED;
+    _last_error = cy_socket_disconnect(socket, 0);
+    socket_assert(_last_error);
+    _status = SOCKET_STATUS_DISCONNECTED;
 
-    s_last_error = cy_socket_delete(socket);
-    socket_assert(s_last_error);
-    s_status = SOCKET_STATUS_CLOSED;
+    _last_error = cy_socket_delete(socket);
+    socket_assert(_last_error);
+    _status = SOCKET_STATUS_DELETED;
 
-    s_last_error = Socket::global_sockets_deinit();
+    _last_error = Socket::global_sockets_deinit();
 }
 
 void Socket::set_timeout(uint32_t timeout) {
-    s_last_error = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
+    _last_error = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
                                     CY_SOCKET_SO_RCVTIMEO, &timeout,
                                     sizeof(timeout)); 
-    socket_assert(s_last_error);
+    socket_assert(_last_error);
 }
 
 void Socket::set_connect_opt_callback(cy_socket_callback_t cback, void * arg) {
@@ -61,6 +54,10 @@ void Socket::set_connect_opt_callback(cy_socket_callback_t cback, void * arg) {
 
 void Socket::set_receive_opt_callback(cy_socket_callback_t cback, void * arg) {
     set_opt_callback(CY_SOCKET_SO_RECEIVE_CALLBACK, cback, arg);
+}
+
+void Socket::set_disconnect_opt_callback(cy_socket_callback_t cback, void * arg) {
+    set_opt_callback(CY_SOCKET_SO_DISCONNECT_CALLBACK, cback, arg);
 }
 
 void Socket::bind(uint16_t port) {
@@ -72,10 +69,12 @@ void Socket::bind(uint16_t port) {
         }
     };
 
-    s_last_error = cy_socket_bind(socket, &address, sizeof(cy_socket_sockaddr_t));
-    socket_assert(s_last_error);
+    _last_error = cy_socket_bind(socket, &address, sizeof(cy_socket_sockaddr_t));
+    socket_assert(_last_error);
 
-    s_status = SOCKET_STATUS_BOUND;
+    _port = port;
+
+    _status = SOCKET_STATUS_BOUND;
 }
 
 bool Socket::connect(IPAddress ip, uint16_t port) {
@@ -88,50 +87,76 @@ bool Socket::connect(IPAddress ip, uint16_t port) {
         }
     };
 
-    s_last_error = cy_socket_connect(socket, &address, sizeof(cy_socket_sockaddr_t));
-    if(s_last_error != CY_RSLT_SUCCESS) {
+    return connect(&address);
+}
+
+bool Socket::connect(const char *host, uint16_t port) {
+    cy_socket_sockaddr_t address = {
+        .port = port, 
+        .ip_address = {
+            .version = CY_SOCKET_IP_VER_V4
+        }
+    };
+    _last_error = cy_socket_gethostbyname(host, CY_SOCKET_IP_VER_V4, &(address.ip_address));
+    if(_last_error != CY_RSLT_SUCCESS) {
+        _status = SOCKET_STATUS_ERROR;
         return false;
     }
 
-    s_status = SOCKET_STATUS_CONNECTED;
-    return true;
+    return connect(&address);
 }
 
 void Socket::listen(int max_connections) {
-    s_last_error = cy_socket_listen(socket, max_connections);
-    socket_assert(s_last_error);
+    _last_error = cy_socket_listen(socket, max_connections);
+    socket_assert(_last_error);
 
-    s_status = SOCKET_STATUS_LISTENING;
+    _status = SOCKET_STATUS_LISTENING;
 }
 
 bool Socket::accept(Socket & client_socket) {
-    /**
-     * TODO: check if we need to keep track of the addresses,
-     * and of the port number in the accepted socket */
     cy_socket_sockaddr_t peer_addr;
     uint32_t peer_addr_len;
 
-    s_last_error = cy_socket_accept(socket, &peer_addr, &peer_addr_len, &(client_socket.socket));
-    if(s_last_error != CY_RSLT_SUCCESS) {
-        s_status = SOCKET_STATUS_ERROR;
+    _last_error = cy_socket_accept(socket, &peer_addr, &peer_addr_len, &(client_socket.socket));
+    if(_last_error != CY_RSLT_SUCCESS) {
+        _status = SOCKET_STATUS_ERROR;
         return false;
     }
 
-    client_socket.s_status = SOCKET_STATUS_CONNECTED;
+    /**
+     * This is usually done in socket.begin(). 
+     * But that is not happening for accepted socket. 
+     */
+    Socket::global_socket_count++;
+
+    client_socket.remote_ip = IPAddress(peer_addr.ip_address.ip.v4);
+    client_socket._port = this->_port;
+    client_socket._status = SOCKET_STATUS_CONNECTED;
+
     return true;
 }
 
 uint32_t Socket::send(const void * data, uint32_t len) {
     uint32_t bytes_sent = 0;
-    s_last_error =  cy_socket_send(socket, data, len,
+    _last_error =  cy_socket_send(socket, data, len,
                                 CY_SOCKET_FLAGS_NONE, &bytes_sent);
-    socket_status_update(s_last_error);
+    if(_last_error != CY_RSLT_SUCCESS) {
+        _status = SOCKET_STATUS_ERROR;
+    }
 
     return bytes_sent;
 }
 uint32_t Socket::available(){
     return rx_buf.available();
 }
+
+int Socket::peek() {
+    if (rx_buf.available() == 0) {
+        return -1;
+    }
+    
+    return rx_buf.peek();
+} 
 
 uint32_t Socket::receive(uint8_t * data, uint32_t len) {
     uint32_t bytes_to_read = (uint32_t)rx_buf.available() > len ? len : rx_buf.available();
@@ -143,23 +168,49 @@ uint32_t Socket::receive(uint8_t * data, uint32_t len) {
     return bytes_to_read;
 }
 
+void Socket::flush() {
+    rx_buf.clear();
+}
+
+IPAddress Socket::remoteIP() {
+    return remote_ip;
+}
+
+uint16_t Socket::port() {
+    return _port;
+}
+
 uint8_t Socket::status() {
-    return s_status;
+    return _status;
 }   
 
 cy_rslt_t Socket::get_last_error() {
-    return s_last_error;
+    return _last_error;
 }   
 
-void Socket::receive_callback() {
+bool Socket::connect(cy_socket_sockaddr_t * addr) {
+    _last_error = cy_socket_connect(socket, addr, sizeof(cy_socket_sockaddr_t));
+    if(_last_error != CY_RSLT_SUCCESS) {
+        _status = SOCKET_STATUS_ERROR;
+        return false;
+    }
+
+    remote_ip = IPAddress(addr->ip_address.ip.v4);
+    _port = addr->port; 
+
+    _status = SOCKET_STATUS_CONNECTED;
+    return true;
+}
+
+void Socket::receiveCallback() {
     if (!rx_buf.isFull()) {
         uint32_t bytes_rcvd_request = rx_buf.availableForStore();
         uint8_t temp_rx_buff [bytes_rcvd_request] = {0};
 
         uint32_t bytes_received = 0;
-        s_last_error = cy_socket_recv(socket, temp_rx_buff, bytes_rcvd_request,
+        _last_error = cy_socket_recv(socket, temp_rx_buff, bytes_rcvd_request,
                                     CY_SOCKET_FLAGS_NONE, &bytes_received);
-        socket_assert(s_last_error);
+        socket_assert(_last_error);
 
         for(uint32_t i = 0; i < bytes_received; i++) {
             rx_buf.store_char(temp_rx_buff[i]);
@@ -173,10 +224,10 @@ void Socket::set_opt_callback(int optname, cy_socket_callback_t cback, void * ar
     cy_opt_callback.callback = cback; 
     cy_opt_callback.arg = arg;
 
-    s_last_error = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
+    _last_error = cy_socket_setsockopt(socket, CY_SOCKET_SOL_SOCKET,
                                     optname,
                                     &cy_opt_callback, sizeof(cy_socket_opt_callback_t));
-    socket_assert(s_last_error); 
+    socket_assert(_last_error); 
 }
 
 bool Socket::global_socket_initialized = false;
--- libraries/WiFi/src/WiFiServer.cpp
+++ libraries/WiFi/src/WiFiServer.cpp
@@ -1,20 +1,20 @@
 #include <WiFiServer.h>
-#include <Arduino.h>
 
 #define wifi_server_assert(cy_ret)   if (cy_ret != CY_RSLT_SUCCESS) { \
         return; \
 }
 
 WiFiServer::WiFiServer(){
-
+    connected_clients.reserve(SERVER_MAX_CLIENTS);
 }
 
 void WiFiServer::begin(uint16_t port) {
     socket.begin();
     
     socket.set_timeout(SERVER_RECV_TIMEOUT_MS);
-    socket.set_connect_opt_callback(connection_callback, this);
-    socket.set_receive_opt_callback(receive_callback, this);
+    socket.set_connect_opt_callback(connectionCallback, this);
+    socket.set_receive_opt_callback(receiveCallback, this);
+    socket.set_disconnect_opt_callback(disconnectionCallback, this);
 
     socket.bind(port);
     socket.listen(SERVER_MAX_CLIENTS);
@@ -24,67 +24,135 @@ void WiFiServer::begin() {
     begin(0);
 }
 
-WiFiClient WiFiServer::available() {
-    WiFiClient client;
+WiFiClient & WiFiServer::available() {
+    /** 
+     * Keep track of the last returned available client. 
+     * Subsequent calls to available would return different 
+     * client objects pointers.
+     * If the returned client is the same you can compare 
+     * using the WiFiClient::operator== or directly the pointer value.
+     */
+    static uint16_t last_available_client = 0;
+    last_available_client = last_available_client % connected_clients.size();
 
-    if (connected_clients.size() > 0) {
-        client = connected_clients.front();
-        /* If client connected and data available */
-        if (client) {
-            return client;
+    for (uint16_t i = last_available_client; i < connected_clients.size(); i++) {
+        if (connected_clients[i]) {
+            last_available_client = i;  
+            return connected_clients[i];
         }
-
-        //We cannot delete it, because we have to keep in the 
-        // list for received connections. 
-        // We can only deleted from the list when disconnecting.
-        // connected_clients.erase(connected_clients.begin());
-        /**
-         * TODO: We need to check later how to handle the client
-         * removal from the list upon disconnection etc.*/
     }
 
-    return client;
+    /** 
+     * If there is no available client, this sentinel object is returned. 
+     * As required by the specification, it will evaluate to false 
+     * in a if-statement. 
+     */
+    static WiFiClient unavailable_client;
+    return unavailable_client;
 }
 
-size_t WiFiServer::write(uint8_t) {
-    return 0;
+size_t WiFiServer::write(uint8_t data) {
+    return write(&data, 1);
+}
+
+size_t WiFiServer::write(const uint8_t *buf, size_t size) {
+    size_t written_bytes = 4294967295; /* Maximum value of uint32_t */
+
+    for (WiFiClient & client: connected_clients) {
+        uint16_t written_bytes_i = client.write(buf, size);
+        /**
+         * Keep as written_bytes the smallest value of written bytes to any clients.
+         *  
+         * If the size passed by argument is equal to the written bytes
+         * for all the connecting clients, that value can be used to check that all the
+         * all the bytes passed to write() have been written to all clients successfully.
+         * 
+         * If not, the information is that at least one of the clients have not been written
+         * successfully all specified bytes.
+         * The there is no way to know which client has received the least bytes,
+         * neither to know which if/which other clients have received all the expected bytes.
+         * 
+         * This is a limitation of the API function. 
+         * The user can always opt to communicate with the client via its own WiFiClient object.
+         */
+        written_bytes = written_bytes_i > written_bytes ? written_bytes : written_bytes_i;
+    }
+
+    return written_bytes;
+}
+
+uint8_t WiFiServer::status() {
+    return socket.status();
 }
 
 void WiFiServer::end() {
-
-    /**
-     * TODO: We need to disconnect all the open connected socket.
-     * */
-    // ret = cy_socket_shutdown(socket, CY_SOCKET_SHUT_RDWR);
-    // wifi_server_assert(ret);
+    for (uint16_t i = 0; i < connected_clients.size(); i++) {
+        connected_clients[i].stop();
+        connected_clients.erase(connected_clients.begin() + i);
+    }
 
     socket.end();
 }
 
-cy_rslt_t WiFiServer::connection_callback(cy_socket_t socket_handle, void *arg) {
-    WiFiServer *server = (WiFiServer *)arg;
-
-    WiFiClient new_client;
-    if(!server->socket.accept(new_client.socket)) {
-        return server->socket.get_last_error();
+WiFiServer::operator bool() {
+    if (socket.status() == SOCKET_STATUS_LISTENING) {
+        return true;
     }
 
-    server->connected_clients.push_back(new_client);
+    return false;
+}
+
+uint8_t WiFiServer::connectedSize() {
+    return connected_clients.size();
+}
+
+cy_rslt_t WiFiServer::connectionCallback(cy_socket_t socket_handle, void *arg) {
+    WiFiServer *server = (WiFiServer *)arg;
+
+    /** 
+     * Add a client to the end of the vector 
+     * and use the reference to accept the connection.  
+     */
+    server->connected_clients.emplace_back();
+    WiFiClient & new_client = server->connected_clients.back();
+
+    if(!server->socket.accept(*(new_client.socket))) {
+        server->connected_clients.pop_back();
+        return server->socket.get_last_error();
+    }
 
     return CY_RSLT_SUCCESS;
 }
 
-cy_rslt_t WiFiServer::receive_callback(cy_socket_t socket_handle, void * arg) {
+cy_rslt_t WiFiServer::receiveCallback(cy_socket_t socket_handle, void * arg) {
     WiFiServer *server = (WiFiServer *)arg;
 
     /** 
      * Find the client which has triggered the callback
      * and receive the data from the client. 
      */
-    std::vector<WiFiClient>::iterator client = server->connected_clients.begin();    
-    for( ;client != server->connected_clients.end(); client++) {
-        if(client->socket.socket == socket_handle) {
-            client->socket.receive_callback();
+    for(WiFiClient & client: server->connected_clients) {
+        if(client.isThisClient(socket_handle)) {
+            client.socket->receiveCallback();
+            break;  
+        }
+    }
+
+    return CY_RSLT_SUCCESS;
+}
+
+cy_rslt_t WiFiServer::disconnectionCallback(cy_socket_t socket_handle, void * arg) {
+    WiFiServer *server = (WiFiServer *)arg;
+
+    /** 
+     * Find the client which has triggered the callback
+     * and stop the client.
+     */
+    for (uint16_t i = 0; i < server->connected_clients.size(); i++) {
+        WiFiClient & disconnecting_client = server->connected_clients[i];
+        if(disconnecting_client.isThisClient(socket_handle)) {
+            disconnecting_client.stop();
+            server->connected_clients.erase(server->connected_clients.begin() + i);
             break;
         }
     }
