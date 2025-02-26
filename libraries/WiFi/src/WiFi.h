f1835410db60404db340ef927e6dd52e0b3d45fa
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-02-26 09:31:35 +0100

    libraries/WiFi/src/WiFi: Added gatewayIP() function.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- libraries/WiFi/src/WiFi.h
+++ libraries/WiFi/src/WiFi.h
@@ -10,7 +10,11 @@
 #include "cy_wcm.h"
 
 /**
+<<<<<<< HEAD
  * TODOs:
+=======
+ * TODO: 
+>>>>>>> 391752a (libraries/WiFi/src/WiFi: Added gatewayIP() function.)
  *  - Adapt the enum list conveniently. Do we need to keep all?
  *  - Move it to some header file?
 */
@@ -80,22 +84,29 @@ public:
     */
     IPAddress localIP();
 
+    /*
+    * Get the gateway IP address.
+    *
+    * return: gateway IP address value
+    */
+    IPAddress gatewayIP();
+
     /**
-     * Return Connection status
-     * TODO: Clean up unused or irrelevant status codes for this core.
-     *
-     *  WL_CONNECTED: assigned when connected to a WiFi network;
-     *  WL_AP_CONNECTED : assigned when a device is connected in Access Point mode;
-     *  WL_AP_LISTENING : assigned when the listening for connections in Access Point mode;
-     *  WL_NO_SHIELD: assigned when no WiFi shield is present;
-     *  WL_NO_MODULE: assigned when the communication with an integrated WiFi module fails;
-     *  WL_IDLE_STATUS: it is a temporary status assigned when WiFi.begin() is called and remains active until the number of attempts expires (resulting in WL_CONNECT_FAILED) or a connection is established (resulting in WL_CONNECTED);
-     *  WL_NO_SSID_AVAIL: assigned when no SSID are available;
-     *  WL_SCAN_COMPLETED: assigned when the scan networks is completed;
-     *  WL_CONNECT_FAILED: assigned when the connection fails for all the attempts;
-     *  WL_CONNECTION_LOST: assigned when the connection is lost;
-     *  WL_DISCONNECTED: assigned when disconnected from a network;
-     */
+        * Return Connection status
+        * TODO: Clean up unused or irrelevant status codes for this core.
+        * 
+        *  WL_CONNECTED: assigned when connected to a WiFi network;
+        *  WL_AP_CONNECTED : assigned when a device is connected in Access Point mode;
+        *  WL_AP_LISTENING : assigned when the listening for connections in Access Point mode;
+        *  WL_NO_SHIELD: assigned when no WiFi shield is present;
+        *  WL_NO_MODULE: assigned when the communication with an integrated WiFi module fails;
+        *  WL_IDLE_STATUS: it is a temporary status assigned when WiFi.begin() is called and remains active until the number of attempts expires (resulting in WL_CONNECT_FAILED) or a connection is established (resulting in WL_CONNECTED);
+        *  WL_NO_SSID_AVAIL: assigned when no SSID are available;
+        *  WL_SCAN_COMPLETED: assigned when the scan networks is completed;
+        *  WL_CONNECT_FAILED: assigned when the connection fails for all the attempts;
+        *  WL_CONNECTION_LOST: assigned when the connection is lost;
+        *  WL_DISCONNECTED: assigned when disconnected from a network;
+        */
     uint8_t status();
 
 private:
