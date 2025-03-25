2791984017face768f9e7a626a4b4f1a526f18b6
IFX-Anusha <Anusha.TR@infineon.com>
2025-03-25 14:44:31 +0530

    libraries/Wire: Ringbuffer implementation.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- libraries/Wire/Wire.cpp
+++ libraries/Wire/Wire.cpp
@@ -10,21 +10,12 @@ extern "C" {
               return; \
 }
 
-TwoWire *TwoWire::instances[I2C_HOWMANY] = {nullptr};
 uint8_t TwoWire::bytesSent = 0;
 
-TwoWire::TwoWire(cyhal_gpio_t sda, cyhal_gpio_t scl, uint8_t instance) : sda_pin(sda), scl_pin(scl), instance(instance) {
-    if (instance < I2C_HOWMANY) {
-        instances[instance] = this;
-    }
+TwoWire::TwoWire(cyhal_gpio_t sda, cyhal_gpio_t scl) : sda_pin(sda), scl_pin(scl) {
 }
 
 void TwoWire::_begin() {
-    rxBufferIndex = 0;
-    rxBufferLength = 0;
-
-    txBufferIndex = 0;
-    txBufferLength = 0;
 
     if (is_master) {
         i2c_config = {
@@ -47,9 +38,9 @@ void TwoWire::_begin() {
 
     if (!is_master) {
         // Configure the read and write buffers for the I2C slave
-        w_status = cyhal_i2c_slave_config_read_buffer(&i2c_obj,  txBuffer, BUFFER_LENGTH);
+        w_status = cyhal_i2c_slave_config_read_buffer(&i2c_obj,  txBuffer._aucBuffer, BUFFER_LENGTH);
         Wire_assert(w_status);
-        w_status = cyhal_i2c_slave_config_write_buffer(&i2c_obj, rxBuffer, BUFFER_LENGTH);
+        w_status = cyhal_i2c_slave_config_write_buffer(&i2c_obj, rxBuffer._aucBuffer, BUFFER_LENGTH);
         Wire_assert(w_status);
     }
 
@@ -69,7 +60,6 @@ void TwoWire::begin(uint8_t address) {
 
 void TwoWire::end() {
     cyhal_i2c_free(&i2c_obj);
-    instances[instance] = nullptr;   // Clear the instance
 }
 
 void TwoWire::setClock(uint32_t freq) {
@@ -91,20 +81,19 @@ void TwoWire::setClock(uint32_t freq) {
 
 void TwoWire::beginTransmission(uint8_t address) {
     txAddress = address;
-    txBufferIndex = 0;
-    txBufferLength = 0;
+    txBuffer.clear();
 }
 
 uint8_t TwoWire::endTransmission(bool sendStop) {
     cy_rslt_t result;
-    bytesSent = txBufferLength;
-    if (txBufferLength == 0) {
-        result = cyhal_i2c_master_write(&i2c_obj, txAddress, txBuffer, 0, timeout, sendStop);
-    } else {
-        result = cyhal_i2c_master_write(&i2c_obj, txAddress, txBuffer, txBufferLength, timeout, sendStop);
-        txBufferIndex = 0;
-        txBufferLength = 0;
+    uint16_t bytes_rcvd_request = txBuffer.available();
+    bytesSent = bytes_rcvd_request;
+    uint8_t temp_tx_buff[bytes_rcvd_request] = {0};
+
+    for (uint16_t i = 0; i < bytes_rcvd_request; i++) {
+        temp_tx_buff[i] = txBuffer.read_char();
     }
+    result = cyhal_i2c_master_write(&i2c_obj, txAddress, temp_tx_buff, bytes_rcvd_request, timeout, sendStop);
     // Handle specific error codes
     switch (result) {
         case I2C_SUCCESS:
@@ -128,11 +117,14 @@ size_t TwoWire::requestFrom(uint8_t address, size_t quantity, bool stopBit) {
     if (quantity > BUFFER_LENGTH) {
         quantity = BUFFER_LENGTH;
     }
-    memset(rxBuffer, 0, BUFFER_LENGTH);
-    cy_rslt_t result = cyhal_i2c_master_read(&i2c_obj, address, rxBuffer, quantity, timeout, stopBit);
+    uint32_t bytes_rcvd_request = rxBuffer.availableForStore();
+    uint8_t temp_rx_buff[bytes_rcvd_request] = {0};
+
+    cy_rslt_t result = cyhal_i2c_master_read(&i2c_obj, address, temp_rx_buff, quantity, timeout, stopBit);
     if (result == CY_RSLT_SUCCESS) {
-        rxBufferIndex = 0;
-        rxBufferLength = quantity;
+        for (uint32_t i = 0; i < quantity; i++) {
+            rxBuffer.store_char(temp_rx_buff[i]);
+        }
         return quantity;   // Return number of bytes read
     } else {
         return 0;   // Return 0 on failure of read operation
@@ -144,18 +136,11 @@ size_t TwoWire::requestFrom(uint8_t address, size_t len) {
 }
 
 size_t TwoWire::write(uint8_t data) {
-    /* Check if buffer is full */
-    if (txBufferLength >= BUFFER_LENGTH) {
-        return 0;
+    if (txBuffer.isFull()) {
+        return 0;   // Buffer is full
     }
 
-    /* Put byte in txBuffer */
-    txBuffer[txBufferIndex] = data;
-    txBufferIndex++;
-
-    /* Update buffer length */
-    txBufferLength = txBufferIndex;
-
+    txBuffer.store_char(data);
     return 1;
 }
 
@@ -177,28 +162,21 @@ size_t TwoWire::write(const char *str) {
 }
 
 int TwoWire::available(void) {
-    return rxBufferLength - rxBufferIndex;
+    return rxBuffer.available();
 }
 
 int TwoWire::read(void) {
-    if (rxBufferIndex < rxBufferLength) {
-        int data = rxBuffer[rxBufferIndex++];
-        if (rxBufferIndex == rxBufferLength) {
-            // Clear the buffer after reading
-            memset(rxBuffer, 0, BUFFER_LENGTH);
-            rxBufferIndex = 0;
-            rxBufferLength = 0;
-        }
-        return data;
+    if (rxBuffer.available() == 0) {
+        return -1;   // Buffer is empty
     }
-    return -1;
+    return rxBuffer.read_char();
 }
 
 int TwoWire::peek(void) {
-    if (rxBufferIndex < rxBufferLength) {
-        return rxBuffer[rxBufferIndex];
+    if (rxBuffer.available() == 0) {
+        return -1;   // Buffer is empty
     }
-    return -1;
+    return rxBuffer.peek();
 }
 
 void TwoWire::onReceive(void (*function)(int)) {
@@ -239,19 +217,24 @@ void TwoWire::onReceiveService(int numBytes) {
     }
 
     // Don't bother if rx buffer is in use by a master requestFrom() op
-    if (rxBufferIndex < rxBufferLength) {
+    if (rxBuffer.available() > 0) {
         return;
     }
 
-    // Manually update rxBufferLength to reflect the number of bytes received
-    rxBufferLength = numBytes;
-    rxBufferIndex = 0;
+    // Manually update rxBuffer to reflect the number of bytes received
+    for (uint8_t i = 0; i < numBytes; i++) {
+        if (i < BUFFER_LENGTH) {
+            rxBuffer.store_char(rxBuffer._aucBuffer[i]);
+        } else {
+            break;
+        }
+    }
 
     // Alert user program
-    user_onReceive(rxBufferLength);
+    user_onReceive(numBytes);
 
     // Reconfigure the write buffer after the transaction
-    w_status = cyhal_i2c_slave_config_write_buffer(&i2c_obj, rxBuffer, BUFFER_LENGTH);
+    w_status = cyhal_i2c_slave_config_write_buffer(&i2c_obj, rxBuffer._aucBuffer, BUFFER_LENGTH);
     Wire_assert(w_status);
 }
 
@@ -261,24 +244,22 @@ void TwoWire::onRequestService(void) {
     if (!user_onRequest) {
         return;
     }
-    // // Reset slave write buffer iterator var
-    txBufferIndex = 0;
-    txBufferLength = 0;
+    // Reset slave write buffer iterator var
+    txBuffer.clear();
 
     // Alert user program
     user_onRequest();
 
     // Reconfigure the read buffer after the transaction
-    w_status = cyhal_i2c_slave_config_read_buffer(&i2c_obj, txBuffer, BUFFER_LENGTH);
+    w_status = cyhal_i2c_slave_config_read_buffer(&i2c_obj, txBuffer._aucBuffer, BUFFER_LENGTH);
     Wire_assert(w_status);
 
 }
 
-
   #if I2C_HOWMANY > 0
-TwoWire Wire = TwoWire(I2C1_SDA_PIN, I2C1_SCL_PIN, 0);
+TwoWire Wire(I2C1_SDA_PIN, I2C1_SCL_PIN);
   #endif
 
   #if I2C_HOWMANY > 1
-TwoWire Wire1 = TwoWire(I2C2_SDA_PIN, I2C2_SCL_PIN, 1);
+TwoWire Wire1(I2C2_SDA_PIN, I2C2_SCL_PIN);
   #endif
