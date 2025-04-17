b2c647cf882aad0c317facfae7df20e46671a931
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-04-17 16:41:51 +0200

    cores/psoc6/Uart: Removed unused and stale structures.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- cores/psoc6/Uart.h
+++ cores/psoc6/Uart.h
@@ -6,12 +6,10 @@
 #include "cyhal_uart.h"
 #include "cyhal_gpio.h"
 
-#define MAX_UARTS    10
-
 class Uart: public arduino::HardwareSerial
 {
 public:
-    static Uart * g_uarts[MAX_UARTS];
+
     Uart(cyhal_gpio_t tx, cyhal_gpio_t rx, cyhal_gpio_t cts, cyhal_gpio_t rts);
     void begin(unsigned long baud);
     void begin(unsigned long baud, uint16_t config);
@@ -23,18 +21,7 @@ public:
     void flush(void);
     virtual size_t write(uint8_t c);
     virtual size_t write(const uint8_t *buffer, size_t size);
-    inline size_t write(unsigned long n) {
-        return write((uint8_t)n);
-    }
-    inline size_t write(long n) {
-        return write((uint8_t)n);
-    }
-    inline size_t write(unsigned int n) {
-        return write((uint8_t)n);
-    }
-    inline size_t write(int n) {
-        return write((uint8_t)n);
-    }
+
     using Print::write;
     operator bool() {
         return true;
@@ -44,7 +31,6 @@ public:
 
 private:
 
-    static constexpr size_t BUFFER_LENGTH = 512;
     cyhal_gpio_t tx_pin;
     cyhal_gpio_t rx_pin;
     cyhal_gpio_t cts_pin;
@@ -52,11 +38,12 @@ private:
     cyhal_uart_t uart_obj;
     cyhal_uart_cfg_t uart_config;
     uint32_t actualbaud;
-    cy_rslt_t result;
+
     uint32_t extractStopBit(uint16_t config);
     uint32_t extractParity(uint16_t config);
     uint32_t extractDataBits(uint16_t config);
 
+    static constexpr size_t BUFFER_LENGTH = 512;
     arduino::RingBufferN < BUFFER_LENGTH > rx_buffer;
 
     void IrqHandler();
