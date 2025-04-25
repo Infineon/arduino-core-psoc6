2b6a746b3741f3ff9b2e28cee89b10695b375558
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2025-04-25 11:22:04 +0530

    ./tests: Add test pins for ADC tests.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

--- tests/test_config.h
+++ tests/test_config.h
@@ -12,13 +12,18 @@
 
 #include <stdint.h>
 
-// test defines
-const uint8_t TEST_DIGITALIO_OUTPUT = 7;  // IO_4
-const uint8_t TEST_DIGITALIO_INPUT = 6;   // IO_3
+// Test Pin Definitions
+#define         TEST_DIGITALIO_OUTPUT   7       // IO_4
+#define         TEST_DIGITALIO_INPUT    6       // IO_3
 
+#define         TEST_SYNC_INPUT_OUTPUT  4       // IO_1
 
-const uint8_t TEST_SYNC_INPUT_OUTPUT = 4;   // IO_1
+#define         TEST_SPI_SLAVE_SELECT   3       // IO_0
 
-const uint8_t TEST_SPI_SLAVE_SELECT = 3;   // IO_0
+#define         TEST_ANALOG_IO_VREF     A0      // Pin connected to VREF
+#define         TEST_ANALOG_IO_DIVIDER  A1      // Pin connected to voltage divider
+
+#define         VDDA                    3.3f    // Assume VDDA is 3.3V
+#define         RESOLUTION              2048    // 11-bit resolution
 
 #endif // TEST_CONFIG_H
