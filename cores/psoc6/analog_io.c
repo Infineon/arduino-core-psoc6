35ffb2a1e46653a3ac4f07c921aa90e3f722f12e
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2025-05-02 23:19:18 +0530

    cores/psoc6: variants/CY8CKIT-062S2-AI: Implement Analog write.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

--- cores/psoc6/analog_io.c
+++ cores/psoc6/analog_io.c
@@ -1,24 +1,42 @@
 #include "Arduino.h"
 #include "cyhal_adc.h"
+#include "cyhal_pwm.h"
 
 // Constants and Macros
+
 #define MIN_ACQUISITION_TIME        220
 #define adc_assert(cy_ret)          if (cy_ret != CY_RSLT_SUCCESS) { return 0xff; }
+#define pwm_assert(cy_ret)          if (cy_ret != CY_RSLT_SUCCESS) { return; }
+
+#define PWM_RESOLUTION_8_BIT  255.0
+#define PWM_RESOLUTION_10_BIT 1023.0
+#define PWM_RESOLUTION_12_BIT 4095.0
+#define PWM_RESOLUTION_16_BIT 65535.0
 
 // Type Definitions
+
 typedef struct {
     pin_size_t pin;
     cyhal_adc_channel_t chan_obj;
     bool initialized;
 } adc_channel_t;
 
+typedef struct {
+    cyhal_pwm_t pwm_obj;
+    pin_size_t pin;
+    bool initialized;
+} pwm_t;
+
 // Static Variables
+
 static cyhal_adc_t adc_obj = {0};
 static cyhal_adc_vref_t desiredVRef = CYHAL_ADC_REF_VDDA;
 static bool adc_initialized = false;
 static adc_channel_t adc_channel[ADC_HOWMANY] = {0};
+static float desiredWriteResolution = PWM_RESOLUTION_8_BIT;
+static pwm_t pwm[PWM_HOWMANY] = {0};
 
-// Helper Function: Initialize ADC hardware if not already done
+
 static cy_rslt_t initialize_adc(pin_size_t pinNumber) {
     if (!adc_initialized) {
         cy_rslt_t status = cyhal_adc_init(&adc_obj, mapping_gpio_pin[pinNumber], NULL);
@@ -88,4 +106,57 @@ int analogRead(pin_size_t pinNumber) {
 
 void analogReference(uint8_t mode) {
     desiredVRef = CYHAL_ADC_REF_VDDA;
+}
+
+void analogWriteResolution(int res) {
+    if (res > 12) {
+        desiredWriteResolution = PWM_RESOLUTION_16_BIT;
+    } else if (res > 10) {
+        desiredWriteResolution = PWM_RESOLUTION_12_BIT;
+    } else if (res > 8) {
+        desiredWriteResolution = PWM_RESOLUTION_10_BIT;
+    } else {
+        desiredWriteResolution = PWM_RESOLUTION_8_BIT;
+    }
+}
+
+void analogWrite(pin_size_t pinNumber, int value) {
+    uint8_t pwm_index = 0;
+    cy_rslt_t result = CY_RSLT_TYPE_ERROR;
+
+    if (pinNumber > GPIO_PIN_COUNT) {
+        return; // Invalid pin number
+    }
+
+    // Find existing channel or initialize a new one
+    for (pwm_index = 0; pwm_index < PWM_HOWMANY; pwm_index++) {
+        if (pwm[pwm_index].initialized) {
+            if (pwm[pwm_index].pin == pinNumber) {
+                // Found an existing channel for the pin
+                break;
+            }
+        } else {
+            // Found an uninitialized channel, initialize it
+            result = cyhal_pwm_init(&pwm[pwm_index].pwm_obj, mapping_gpio_pin[pinNumber], NULL);
+            pwm_assert(result);
+            pwm[pwm_index].pin = pinNumber;
+            pwm[pwm_index].initialized = true;
+            break;
+        }
+    }
+
+    if (value < 0) {
+        value = 0;
+    }
+    if (value > desiredWriteResolution) {
+        value = desiredWriteResolution;
+    }
+
+    float duty_cycle_pertentage = (value / desiredWriteResolution) * 100.0f;
+
+    result = cyhal_pwm_set_duty_cycle(&pwm[pwm_index].pwm_obj, duty_cycle_pertentage, PWM_FREQUENCY_HZ);
+    pwm_assert(result);
+
+    result = cyhal_pwm_start(&pwm[pwm_index].pwm_obj);
+    pwm_assert(result);
 }
