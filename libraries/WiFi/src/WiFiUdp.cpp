35efbb9a0581a72e21c3a956b5c4e4344555e51d
IFX-Anusha <Anusha.TR@infineon.com>
2025-05-23 11:34:53 +0530

    libraries/WiFi: Refactor to handle the packets correctly.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- libraries/WiFi/src/WiFiUdp.cpp
+++ libraries/WiFi/src/WiFiUdp.cpp
@@ -1,4 +1,5 @@
 #include "WiFiUdp.h"
+#include <Arduino.h>
 
 
 #define udp_assert(cy_ret)   if (cy_ret != CY_RSLT_SUCCESS) { \
@@ -14,13 +15,10 @@ WiFiUDP::WiFiUDP() :
     _status(SOCKET_STATUS_UNINITED),
     _last_error(CY_RSLT_SUCCESS),
     remote_ip(0, 0, 0, 0),
-    _port(0),
-    _parsedPacketSize(0) {
+    _port(0) {
 }
 
 uint8_t WiFiUDP::begin(uint16_t port) {
-    _parsedPacketSize = 0;
-
     // Initialize the socket for UDP
     socket.begin(SOCKET_PROTOCOL_UDP);
     if (socket.status() != SOCKET_STATUS_CREATED) {
@@ -62,7 +60,7 @@ int WiFiUDP::beginPacket(const char *host, uint16_t port) {
 }
 
 int WiFiUDP::endPacket() {
-    size_t size = txBuffer.available() > WIFI_UDP_BUFFER_SIZE ? WIFI_UDP_BUFFER_SIZE : txBuffer.available();
+    size_t size = static_cast < size_t > (txBuffer.available()) > WIFI_UDP_BUFFER_SIZE ? WIFI_UDP_BUFFER_SIZE : static_cast < size_t > (txBuffer.available());
     uint8_t temp_buffer[size];
     for (size_t i = 0; i < size; i++) {
         temp_buffer[i] = txBuffer.read_char();
@@ -94,76 +92,76 @@ size_t WiFiUDP::write(const uint8_t *buffer, size_t size) {
 }
 
 int WiFiUDP::parsePacket() {
-    if (_parsedPacketSize > 0) {
-        // discard previously parsed packet data
-        while (available()) {
-            read();
-        }
+
+    // Check if there are any packets in the rx_packets vector
+    if (rx_packets.empty()) {
+        current_packet = {{}, IPAddress(0, 0, 0, 0), 0};
+        return 0;
     }
-    _parsedPacketSize = socket.available();
-    return _parsedPacketSize;
+
+    current_packet = rx_packets.front();
+    rx_packets.erase(rx_packets.begin());
+    return current_packet.rx_buf.available();
 }
 
 int WiFiUDP::available() {
-    return socket.available();
+    return current_packet.rx_buf.available();
 }
 
 int WiFiUDP::read() {
-
-    if (_parsedPacketSize < 1) {
+    if (current_packet.rx_buf.available() < 1) {
         return -1;
     }
-    uint8_t data;
-    uint32_t received_data = socket.receive(&data, 1);
-    if (received_data == 0) {
-        return -1;
-    }
-    _parsedPacketSize--;
-    return (int)data;
+    char buffer[1];
+    read((unsigned char *)&buffer, 1);
+    return buffer[0];
 }
 
 int WiFiUDP::read(unsigned char *buffer, size_t len) {
 
-    if (_parsedPacketSize < 1) {
+    if (current_packet.rx_buf.available() < 1) {
         return -1;
     }
-    uint32_t received_data = socket.receive((uint8_t *)buffer, len);
-    if (received_data == 0) {
-        return -1;
+    for (size_t i = 0; i < len; i++) {
+        buffer[i] = current_packet.rx_buf.read_char();
     }
-    _parsedPacketSize -= received_data;
-    return (int)received_data;
+    return len;
 }
 
 int WiFiUDP::peek() {
-    if (_parsedPacketSize < 1) {
+    if (current_packet.rx_buf.available() < 1) {
         return -1;
     }
-    return socket.peek();
+    return current_packet.rx_buf.peek();
 }
 
 void WiFiUDP::flush() {
-    socket.flush();
-    _parsedPacketSize = 0;
+    current_packet.rx_buf.clear();
 }
 
 IPAddress WiFiUDP::remoteIP() {
-    return _senderIP;
+    return current_packet._senderIP;
 }
 
 uint16_t WiFiUDP::remotePort() {
-    return _senderPort;
+    return current_packet._senderPort;
 }
 
 cy_rslt_t WiFiUDP::receiveCallback(cy_socket_t socket_handle, void *arg) {
     WiFiUDP *udp = (WiFiUDP *)arg;
 
     cy_socket_sockaddr_t peer_addr;
+
     udp->socket.receiveCallback(&peer_addr);
 
-    // store the sender's IP address and port
-    udp->_senderIP = IPAddress(peer_addr.ip_address.ip.v4);
-    udp->_senderPort = peer_addr.port;
+    udp_rx_packet_t new_packet;
+    new_packet._senderIP = IPAddress(peer_addr.ip_address.ip.v4);
+    new_packet._senderPort = peer_addr.port;
 
+    uint32_t bytes_received = udp->socket.rx_buf.available();
+    for (uint32_t i = 0; i < bytes_received; i++) {
+        new_packet.rx_buf.store_char(udp->socket.rx_buf.read_char());
+    }
+    udp->rx_packets.push_back(new_packet);
     return CY_RSLT_SUCCESS;
 }
