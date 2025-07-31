23a3644cd8aac61d9e5c0a4f971b6dc3c898c42f
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2025-07-31 10:22:42 +0530

    ./tests: Move PSOC specific variables from spi test to test_config.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

---
+++ tests/test_config.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file test_config.cpp
+ * @brief Implementation file for board-specific test configuration variables.
+ *
+ */
+
+#include "test_config.h"
+#include <SPI.h>
+
+// Define the SPI master and slave instances
+SPIClassPSOC *spi_master = &SPI; 
+SPIClassPSOC SPI1 = SPIClassPSOC(PIN_SPI_MOSI, PIN_SPI_MISO, PIN_SPI_SCK, TEST_PIN_SPI_SSEL, true);
+SPIClassPSOC *spi_slave = &SPI1;
